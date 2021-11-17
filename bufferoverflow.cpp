903153d2ba7ecf0648727de7633c88fdfc715ce1
efeller1552 <emily@fellerfamily.net>
2021-11-17 09:05:52 -0600

    buffer

---
+++ bufferoverflow.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <sstream>
+//#include "platform/CCFileUtils.h"
+//#include "base/ccUtils.h"
+
+//#include "3d/CCObjLoader.h"
+#define TINYOBJ_SSCANF_BUFFER_SIZE  (4096)
+
+int test(){
+    char value1[512] = "abcdefg ...";
+    char value2[512] = "abcdefgg ...";
+    setError(value1, value2);
+}
+
+void setError(const char *value1, const char *value2) {
+   
+	char message[256];
+	int length;
+	strcpy(message, value1);
+	length = (int) strlen(value1);
+	if (value2) strncat(message + length, value2, 255 - length);
+	
+}
